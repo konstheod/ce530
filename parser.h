94cc18f40ded8fc7e042b1fcdff1c8b2052caf14
konstheo <konstheo@uth.gr>
2016-10-04 23:10:19 +0300

    I change all the nodes from int to long int

--- parser.h
+++ parser.h
@@ -8,12 +8,12 @@ struct element{
 	long int pos;
 	long int neg;
 	double value;
-	int D;
-	int G;
-	int S;
-	int B;
-	int C;
-	int E;
+	long int D;
+	long int G;
+	long int S;
+	long int B;
+	long int C;
+	long int E;
 	double L;
 	double W;
 	char model_name[SIZE_NAME];
@@ -32,13 +32,13 @@ void printList(struct element *head, struct node *hash_head){
 			printf("%d: type = %c, name = %s, pos = %s, neg = %s, value = %.*lf \n",i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), PREC,curr->value);
 		}
 		else if(curr->type == 'D'){
-			printf("%d: type = %c, name = %s, pos = %ld, neg = %ld, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, curr->pos, curr->neg, curr->model_name, PREC, curr->area);
+			printf("%d: type = %c, name = %s, pos = %s, neg = %s, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), curr->model_name, PREC, curr->area);
 		}
 		else if(curr->type == 'M'){
-			printf("%d: type = %c, name = %s, D = %d, G = %d, S = %d, B = %d, model_name = %s, L = %.*lf, W = %.*lf \n", i, curr->type, curr->name, curr->D, curr->G, curr->S, curr->B, curr->model_name, PREC, curr->L, PREC, curr->W);
+			printf("%d: type = %c, name = %s, D = %s, G = %s, S = %s, B = %s, model_name = %s, L = %.*lf, W = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->D), find_value(hash_head,curr->G), find_value(hash_head,curr->S), find_value(hash_head,curr->B), curr->model_name, PREC, curr->L, PREC, curr->W);
 		}
 		else {
-			printf("%d: type = %c, name = %s, C = %d, B = %d, E = %d, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, curr->C, curr->B, curr->E, curr->model_name, PREC, curr->area);
+			printf("%d: type = %c, name = %s, C = %s, B = %s, E = %s, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->C), find_value(hash_head,curr->B), find_value(hash_head,curr->E), curr->model_name, PREC, curr->area);
 		}
 		i++;
 	}
