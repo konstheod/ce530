0325547bb5d37ca8cf4c898c99ed6dc6fb3bc3a9
karakoul <karakoul@uth.gr>
2016-10-05 16:14:14 +0300

    Fixed print

--- parser.h
+++ parser.h
@@ -625,16 +625,16 @@ void printList(struct element *head, struct node *hash_head){
 	
 	for(curr = head; curr != NULL; curr = curr->next){
 		if(curr->type == 'R' || curr->type == 'C' || curr->type == 'L' || curr->type == 'V' || curr->type == 'I'){
-			printf("%d: type = %c, name = %s, pos = %s, neg = %s, value = %.*lf \n",i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), PREC,curr->value);
+			printf("%d: %c%s %s %s %.*lf \n",i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), PREC,curr->value);
 		}
 		else if(curr->type == 'D'){
-			printf("%d: type = %c, name = %s, pos = %s, neg = %s, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), curr->model_name, PREC, curr->area);
+			printf("%d: %c%s %s %s %s %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->pos), find_value(hash_head,curr->neg), curr->model_name, PREC, curr->area);
 		}
 		else if(curr->type == 'M'){
-			printf("%d: type = %c, name = %s, D = %s, G = %s, S = %s, B = %s, model_name = %s, L = %.*lf, W = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->D), find_value(hash_head,curr->G), find_value(hash_head,curr->S), find_value(hash_head,curr->B), curr->model_name, PREC, curr->L, PREC, curr->W);
+			printf("%d: %c%s %s %s %s %s %s L = %.*lf W = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->D), find_value(hash_head,curr->G), find_value(hash_head,curr->S), find_value(hash_head,curr->B), curr->model_name, PREC, curr->L, PREC, curr->W);
 		}
 		else {
-			printf("%d: type = %c, name = %s, C = %s, B = %s, E = %s, model_name = %s, area = %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->C), find_value(hash_head,curr->B), find_value(hash_head,curr->E), curr->model_name, PREC, curr->area);
+			printf("%d: %c%s %s %s %s %s %.*lf \n", i, curr->type, curr->name, find_value(hash_head,curr->C), find_value(hash_head,curr->B), find_value(hash_head,curr->E), curr->model_name, PREC, curr->area);
 		}
 		i++;
 	}
